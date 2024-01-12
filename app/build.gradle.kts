adac341198c804a41cac5b80a7c01fecc594bcc4
mgradyn <mgradyn@gmail.com>
2024-01-12 21:44:19 +0700

    feat: add room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+    implementation("androidx.room:room-common:2.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -74,4 +75,10 @@ dependencies {
     val nav_version_ktx = "2.1.0"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version_ktx")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version_ktx")
+
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
 }
---
+++ app/src/main/java/com/surendramaran/yolov8tflite/database/TreeDatabase.kt
@@ -0,0 +1,37 @@
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.surendramaran.yolov8tflite.database.TreeDao
+import com.surendramaran.yolov8tflite.entities.Tree
+
+@Database(entities = [Tree::class], version = 1)
+abstract class TreeDatabase : RoomDatabase() {
+
+    abstract fun noteDao(): TreeDao
+
+    companion object {
+        private var instance: TreeDatabase? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): TreeDatabase {
+            if(instance == null)
+                instance = Room.databaseBuilder(ctx.applicationContext, TreeDatabase::class.java,
+                    "tree_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
+                    .build()
+
+            return instance!!
+
+        }
+
+        private val roomCallback = object : Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+            }
+        }
+
+    }
+}
