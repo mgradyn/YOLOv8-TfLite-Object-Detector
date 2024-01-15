89d1287e7fd6992c882a7e5a92ce43a614815cef
Martinus Grady <48128978+mgradyn@users.noreply.github.com>
2024-01-15 18:29:17 +0700

    fix: tidy list layout

--- app/src/main/java/com/surendramaran/yolov8tflite/adapter/TreeCardAdapter.kt
+++ app/src/main/java/com/surendramaran/yolov8tflite/adapter/TreeCardAdapter.kt
@@ -3,6 +3,7 @@ package com.surendramaran.yolov8tflite.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
@@ -22,7 +23,14 @@ class TreeCardAdapter() : RecyclerView.Adapter<TreeCardAdapter.CardViewHolder>()
     class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val cardView: MaterialCardView = itemView.findViewById(R.id.treeCardView)
         val textTitle: TextView = itemView.findViewById(R.id.textTitle)
-        val textContent: TextView = itemView.findViewById(R.id.textContent)
+        val textLatitude: TextView = itemView.findViewById(R.id.textLatitude)
+        val textLongitude: TextView = itemView.findViewById(R.id.textLongitude)
+        val textRipe: TextView = itemView.findViewById(R.id.textRipe)
+        val textUnderripe: TextView = itemView.findViewById(R.id.textUnderripe)
+        val textUnripe: TextView = itemView.findViewById(R.id.textUnripe)
+        val textFlower: TextView = itemView.findViewById(R.id.textFlower)
+        val textAbnormal: TextView = itemView.findViewById(R.id.textAbnormal)
+        val countDetailContainer: LinearLayout = itemView.findViewById(R.id.countDetailContainer)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
@@ -35,12 +43,18 @@ class TreeCardAdapter() : RecyclerView.Adapter<TreeCardAdapter.CardViewHolder>()
         val item = cardItems?.get(position)
         item?.let {
             holder.textTitle.text = it.id
-            holder.textContent.text = it.latitude.toString()
+            holder.textLatitude.text = it.latitude.toString()
+            holder.textLongitude.text = it.longitude.toString()
+            holder.textRipe.text = it.ripe.toString()
+            holder.textUnderripe.text = it.underripe.toString()
+            holder.textUnripe.text = it.unripe.toString()
+            holder.textFlower.text = it.flower.toString()
+            holder.textAbnormal.text = it.abnromal.toString()
 
             // Add click listener to handle expansion/collapse
             holder.cardView.setOnClickListener {
-                val expanded = holder.textContent.visibility == View.VISIBLE
-                holder.textContent.visibility = if (expanded) View.GONE else View.VISIBLE
+                val expanded = holder.countDetailContainer.visibility == View.VISIBLE
+                holder.countDetailContainer.visibility = if (expanded) View.GONE else View.VISIBLE
             }
         }
     }
